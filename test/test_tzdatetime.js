54d4499a9ecb6a81a5111c9da4f90dd957f3c5ce
Frank Boës <frank.boes@shift.agency>
2016-08-05 10:59:00 +0200

    Fixed timezone dependency of test

--- test/test_tzdatetime.js
+++ test/test_tzdatetime.js
@@ -5,7 +5,8 @@ var dateFormat = require('./../lib/dateformat');
 describe('isoUtcDateTime', function() {
   it('should correctly format the timezone part', function(done) {
     var actual = dateFormat('2014-06-02T13:23:21-08:00', 'yyyy-mm-dd\'T\'HH:MM:ssp');
-    assert.strictEqual(actual, '2014-06-02T23:23:21+02:00');
+    var actual2 = dateFormat('2014-06-02T13:23:21-08:00', 'yyyy-mm-dd\'T\'HH:MM:sso');
+    assert.strictEqual(actual, actual2.replace(/(\d\d)$/, ':$1'));
     done();
   });
 });
