fb161068482e97790e97a2f90f409e3154ed7e0d
Andrey Nikolaev <anikolaev@productengine.com>
2017-06-23 15:29:36 +0300

    Fix incorrect work with double-quoted substrings. Their content was not ignored.

---
+++ test/test_quotes.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+
+var dateFormat = require('./../lib/dateformat');
+
+describe('quoted substrings', function() {
+  var az = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  it('should not format single quoted substrings removing quotes', function() {
+    var result = dateFormat("'" + az + "'");
+    assert.strictEqual(result, az);
+  });
+
+  it('should not format double quoted substrings removing quotes', function() {
+    var result = dateFormat('"' + az + '"');
+    assert.strictEqual(result, az);
+  });
+});
+
