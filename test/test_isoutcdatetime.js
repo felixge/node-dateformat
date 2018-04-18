00ffba46d9dc4b0f4fab386e5254f607906b6af1
hooriza <hooriza@gmail.com>
2018-04-18 17:34:15 +0900

    changed to use the epoch time if first arguments is 0. https://github.com/felixge/node-dateformat/issues/79

--- test/test_isoutcdatetime.js
+++ test/test_isoutcdatetime.js
@@ -6,6 +6,8 @@ describe('isoUtcDateTime', function() {
   it('should correctly format the timezone part', function(done) {
     var actual = dateFormat('2014-06-02T13:23:21-08:00', 'isoUtcDateTime');
     assert.strictEqual(actual, '2014-06-02T21:23:21Z');
+    var epochTime = dateFormat(0, 'isoUtcDateTime');
+    assert.strictEqual(epochTime, '1970-01-01T00:00:00Z');
     done();
   });
 });
