5fbbe1d72b2b12c1af40441bb367656d7dc18ec3
Chase Manning <me@chasemanning.co.nz>
2020-11-10 17:58:08 +1300

    Add Mask 'd' Test

---
+++ test/test_mask-d.js
@@ -0,0 +1,32 @@
+var assert = require("assert");
+var dateFormat = require("../lib/dateformat");
+
+describe("Mask: 'd'", function () {
+  it("should format '1993-03-12' as '12'", function (done) {
+    let date = new Date("1993-03-12");
+    var d = dateFormat(date, "d");
+    assert.strictEqual(d, "12");
+    done();
+  });
+
+  it("should format '2020-11-1' as '1'", function (done) {
+    let date = new Date("2020-11-1");
+    var d = dateFormat(date, "d");
+    assert.strictEqual(d, "1");
+    done();
+  });
+
+  it("should format '1830-01-20' as '20'", function (done) {
+    let date = new Date("1830-01-20");
+    var d = dateFormat(date, "d");
+    assert.strictEqual(d, "20");
+    done();
+  });
+
+  it("should not format '1830-01-06' as '06'", function (done) {
+    let date = new Date("1830-01-20");
+    var d = dateFormat(date, "d");
+    assert.notStrictEqual(d, "06");
+    done();
+  });
+});
