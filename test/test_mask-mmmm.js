a303c4b388920054f3bb5c2b17d9438b745eb5d5
Chase Manning <me@chasemanning.co.nz>
2020-11-10 18:27:44 +1300

    Create Mask 'mmmm' Tests

---
+++ test/test_mask-mmmm.js
@@ -0,0 +1,25 @@
+var assert = require("assert");
+var dateFormat = require("../lib/dateformat");
+
+describe("Mask: 'mmmm'", function () {
+  it("should format '1993-02-11' as 'February'", function (done) {
+    let date = new Date("1993-02-11");
+    var d = dateFormat(date, "mmmm");
+    assert.strictEqual(d, "February");
+    done();
+  });
+
+  it("should format '2023-11-13' as 'November'", function (done) {
+    let date = new Date("2023-11-13");
+    var d = dateFormat(date, "mmmm");
+    assert.strictEqual(d, "November");
+    done();
+  });
+
+  it("should format '2077-10-01' as 'October'", function (done) {
+    let date = new Date("2077-10-01");
+    var d = dateFormat(date, "mmmm");
+    assert.strictEqual(d, "October");
+    done();
+  });
+});
