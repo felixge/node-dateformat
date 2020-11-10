7c2006c0e7b609364d61a08be49e4552797a82b1
Chase Manning <me@chasemanning.co.nz>
2020-11-10 19:20:35 +1300

    Create Mask 'N' Tests

---
+++ test/test_mask-n_.js
@@ -0,0 +1,36 @@
+var assert = require("assert");
+var dateFormat = require("../lib/dateformat");
+
+describe("Mask: 'N'", function () {
+  it("should format '1984-02-7' as '2'", function (done) {
+    let date = new Date("1984-02-7");
+    var d = dateFormat(date, "N");
+    assert.strictEqual(d, "2");
+    done();
+  });
+
+  it("should format '2013-01-17' as '4'", function (done) {
+    let date = new Date("2013-01-17");
+    var d = dateFormat(date, "N");
+    assert.strictEqual(d, "4");
+    done();
+  });
+
+  it("should format '2034-11-24' as '5'", function (done) {
+    var d = dateFormat("2034-11-24", "N");
+    assert.strictEqual(d, "5");
+    done();
+  });
+
+  it("should format '2002-02-3' as '7'", function (done) {
+    var d = dateFormat("2002-02-3", "N");
+    assert.strictEqual(d, "7");
+    done();
+  });
+
+  it("should format '2002-02-4' as '1'", function (done) {
+    var d = dateFormat("2002-02-4", "N");
+    assert.strictEqual(d, "1");
+    done();
+  });
+});
