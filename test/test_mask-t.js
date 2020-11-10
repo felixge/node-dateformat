0f380fe9d7c2b820ce59b500f1c12a2365920878
Chase Manning <me@chasemanning.co.nz>
2020-11-10 21:10:04 +1300

    Create Mask 't' Tests

---
+++ test/test_mask-t.js
@@ -0,0 +1,30 @@
+var assert = require("assert");
+var dateFormat = require("../lib/dateformat");
+
+describe("Mask: 't'", function () {
+  it("should format '1876-03-22T23:08:02.429' as 'p'", function (done) {
+    var date = new Date("1876-03-22T23:08:02.429");
+    var d = dateFormat(date, "t");
+    assert.strictEqual(d, "p");
+    done();
+  });
+
+  it("should format '2013-12-11T05:34:35.350' as 'a'", function (done) {
+    var date = new Date("2013-12-11T05:34:35.350");
+    var d = dateFormat(date, "t");
+    assert.strictEqual(d, "a");
+    done();
+  });
+
+  it("should format '2020-08-29T00:32:00.101' as 'a'", function (done) {
+    var d = dateFormat("2020-08-29T00:32:00.101", "t");
+    assert.strictEqual(d, "a");
+    done();
+  });
+
+  it("should format '2020-09-22T23:04:09.358' as 'p'", function (done) {
+    var d = dateFormat("2020-09-22T23:04:09.358", "t");
+    assert.strictEqual(d, "p");
+    done();
+  });
+});
