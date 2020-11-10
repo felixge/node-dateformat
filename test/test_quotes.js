129d814dcfda52d3ee3f55cfd61c7643cd90eea5
Chase Manning <me@chasemanning.co.nz>
2020-11-10 16:59:55 +1300

    Minor Formatting Changes for Quotes Test

--- test/test_quotes.js
+++ test/test_quotes.js
@@ -1,17 +1,16 @@
-var assert = require('assert');
+var assert = require("assert");
 
-var dateFormat = require('./../lib/dateformat');
+var dateFormat = require("./../lib/dateformat");
 
-describe('quoted substrings', function() {
-  var az = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  it('should not format single quoted substrings removing quotes', function() {
+describe("quoted substrings", function () {
+  var az = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  it("should not format single quoted substrings removing quotes", function () {
     var result = dateFormat("'" + az + "'");
     assert.strictEqual(result, az);
   });
 
-  it('should not format double quoted substrings removing quotes', function() {
+  it("should not format double quoted substrings removing quotes", function () {
     var result = dateFormat('"' + az + '"');
     assert.strictEqual(result, az);
   });
 });
-
