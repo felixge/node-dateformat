a8388c4184cf32498e1ca03604e3949abad968de
Chase Manning <me@chasemanning.co.nz>
2020-11-11 13:46:06 +1300

    Create Benchmark Script

---
+++ benchmark/benchmark.js
@@ -0,0 +1,38 @@
+var previousDateFormat = require("./previousDateFormat");
+var newDateFormat = require("../lib/dateformat");
+
+const masks = [
+  "d",
+  "W",
+  "o",
+  "N",
+  "shortDate",
+  "fullDate",
+  "longTime",
+  "default",
+];
+let results = [];
+
+masks.forEach((mask) => {
+  const previousSpeed = getSpeed(true, mask);
+  const newSpeed = getSpeed(false, mask);
+  results.push({
+    mask: mask,
+    previous: previousSpeed + "ms",
+    new: newSpeed + "ms",
+    improvement: Math.round((previousSpeed / newSpeed - 1) * 100, 2) + "%",
+  });
+});
+
+function getSpeed(newVersion, mask) {
+  const startTime = new Date();
+  const date = new Date();
+  for (var i = 0; i < 100_000; i++) {
+    if (newVersion) newDateFormat(date, mask);
+    else previousDateFormat(date, mask);
+  }
+  const endTime = new Date();
+  return endTime - startTime;
+}
+
+console.table(results);
