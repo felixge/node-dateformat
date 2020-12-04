466088f03366c0ed78b6d2532f78953a59ebbc9a
Chase Manning <me@chasemanning.co.nz>
2020-12-04 19:46:14 +1300

    :white_check_mark: Add Epoch Time Test

--- test/test_isoutcdatetime.js
+++ test/test_isoutcdatetime.js
@@ -1,11 +1,13 @@
-var assert = require('assert');
+var assert = require("assert");
 
-var dateFormat = require('./../lib/dateformat');
+var dateFormat = require("./../lib/dateformat");
 
-describe('isoUtcDateTime', function() {
-  it('should correctly format the timezone part', function(done) {
-    var actual = dateFormat('2014-06-02T13:23:21-08:00', 'isoUtcDateTime');
-    assert.strictEqual(actual, '2014-06-02T21:23:21Z');
+describe("isoUtcDateTime", function () {
+  it("should correctly format the timezone part", function (done) {
+    var actual = dateFormat("2014-06-02T13:23:21-08:00", "isoUtcDateTime");
+    assert.strictEqual(actual, "2014-06-02T21:23:21Z");
+    var epochTime = dateFormat(0, "isoUtcDateTime");
+    assert.strictEqual(epochTime, "1970-01-01T00:00:00Z");
     done();
   });
 });
