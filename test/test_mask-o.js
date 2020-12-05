9d1f2a9d548db7f2f37dd9189c52ba27d354eef4
Frank Boës <fboes@3960.org>
2020-12-05 10:18:22 +0100

    Fixed typo in tests

--- test/test_mask-o.js
+++ test/test_mask-o.js
@@ -2,7 +2,7 @@ var assert = require("assert");
 var dateFormat = require("../lib/dateformat");
 
 describe("Mask: 'o'", function () {
-  it("should get timezone for any date as smoething like [+-]XXXX", function (done) {
+  it("should get timezone for any date as something like [+-]XXXX", function (done) {
     var date = new Date();
     var d = dateFormat(date, "o");
     assert.ok(d.match(/^[+-]\d{4}$/), d);
--- test/test_mask-p.js
+++ test/test_mask-p.js
@@ -2,10 +2,12 @@ var assert = require("assert");
 var dateFormat = require("../lib/dateformat");
 
 describe("Mask: 'p'", function () {
-  it("should get timezone for any date as smoething like [+-]XX:XX", function (done) {
+  it("should get timezone for any date as something like [+-]XX:XX", function (done) {
     var date = new Date();
     var d = dateFormat(date, "p");
     assert.ok(d.match(/^[+-]\d{2}:\d{2}$/), d);
     done();
+
+    console.log( dateFormat(date, "isoDateTime"));
   });
 });
